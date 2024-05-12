dbefbf00bccaeb580b54990f87a5da3fea1b3d0c
bitficker <shopper.tiago@gmail.com>
2024-05-12 15:56:06 -0300

    adjustments

--- Bob.hs
+++ Bob.hs
@@ -6,16 +6,18 @@ responseFor :: String -> String
 
 data Action = Question | Yelling | YellingQuestion | Silence | AnythingElse
 
-
 responseFor said = reaction
     where
         getAction :: String -> Action
         getAction [] = Silence
         getAction (x : xs)
-            | ord x == 32 = silence xs
+            | isEmptySpace asciiChar = silence xs
             | isUppercase x = yelling xs
-            | isSpecialSymbolOrNumber x = specialSymbol xs
+            | isSpecialCharExceptEmptySpace x = shouting xs
             | otherwise = anythingElse xs
+            -- is there no question mark at the beggining of any of the test sentences
+
+        asciiChar = ord x
 
         action = getAction said
         reaction =
@@ -27,38 +29,59 @@ responseFor said = reaction
                 AnythingElse -> "Whatever."
 
 
-yelling :: String -> Action -- Yelling | YellingQuestion | AnythingElse
+yelling :: String -> Action
 yelling (x : []) = if x == '?' then YellingQuestion else Yelling
 yelling (x : xs)
-    | isUppercase x = yelling xs
-    | isSpecialSymbolOrNumber x = yelling xs
+    | (||) (isEmptySpace x) (isUppercase x) -> yelling xs
+    | isSpecialCharExceptEmptySpace x = yelling xs
+    | isQuestionMark x = yellingQuestion xs
     | otherwise = anythingElse xs
 
-anythingElse :: String -> Action -- AnythingElse | Question
+yellingQuestion :: String -> Action
+yellingQuestion [] -> YellingQuestion
+yellingQuestion (x : xs)
+    | isEmptySpace x -> yellingQuestion xs
+    | isQuestionMark x -> yellingQuestion xs
+    | isUppercase x -> yelling xs
+    | otherwise x -> anythingElse xs
+
+anythingElse :: String -> Action
 anythingElse (x : []) = if x == '?' then Question else AnythingElse
 anythingElse (x : xs) = anythingElse xs
 
-silence :: String -> Action -- Silence | AnythingElse
+silence :: String -> Action
 silence [] = Silence
 silence (x : xs)
-    | ord x == 32 = silence xs
-    | isSpecialSymbolOrNumber x = specialSymbol xs
+    | isEmptySpace x = silence xs
+    | isSpecialCharExceptEmptySpace x = shouting xs
     | isUppercase x = yelling xs
---silence (x : xs) = if ord x == 20 then silence xs else anythingElse xs
+    | otherwise = anythingElse xs
 
-specialSymbol :: String -> Action -- Yelling
-specialSymbol (x : []) = if x == '?' then Question else AnythingElse
-specialSymbol (x : xs) = if isSpecialSymbolOrNumber x then specialSymbol xs else yelling xs
 
-isSpecialSymbolOrNumber :: Char -> Bool
-isSpecialSymbolOrNumber l = (&&) (not $ isUppercase l) (isNotLowerCase l)
+shouting :: String -> Action
+shouting  [] = AnythingElse
+shouting  (x : xs) = if isSpecialCharExceptEmptySpace x then shouting xs else yelling xs -- desmembrar isSpecial ...
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+isEmptySpace :: Int -> Bool
+isEmptySpace = (||) (x == 32) (x == 9) $ (||) x == 13 $ (||) x == 10 =  silence xs -- space, horizontal tab (\t), carriage return (\r), line feed (\n)
+
+isQuestionMark :: Char -> Bool
+isQuestionMark c = c == '?'
+
+isSpecialCharExceptEmptySpace :: Char -> Bool
+isSpecialCharExceptEmptySpace c = (&&) (not $ isAlphabet c) (not $ isEmptySpace c)
+
+isAlphabet :: Char -> Bool
+isAlphabet = (&&) (isUppercase l) (isLowerCase l)
 
 isUppercase :: Char -> Bool
-isUppercase c = (&&) ((<=) 65 (ord c)) ((<=) (ord c ) 90) -- 65 - 90
+isUppercase c = (&&) ((<=) 65 (ord c)) ((<=) (ord c ) 90)
 
-isNotLowerCase :: Char -> Bool
-isNotLowerCase l = not ((&&) ((<=) 97 (ord l)) ((<=) (ord l) 122))
-
+isLowerCase :: Char -> Bool
+isLowerCase l = (&&) ((<=) 97 (ord l)) ((<=) (ord l) 122)
 
 
 
